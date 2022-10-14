3d4f4525a0dd9b57f9e26590e1fc98781992d800
sssumeet <sumitmankar05@gmail.com>
2022-10-14 17:52:55 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -17,15 +17,15 @@ public class Main {
 
     public static class Product{
 
-        public int product(int x, int y){
+        public static int product(int x, int y){
             return (x*y);
         }
 
-        public int product(int x, int y, int z){
+        public static int product(int x, int y, int z){
             return (x*y*x);
         }
 
-        public double product(double x, double y){
+        public static double product(double x, double y){
             return (x*y);
         }
 
